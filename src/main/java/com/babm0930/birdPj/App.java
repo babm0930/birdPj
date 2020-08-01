efb72ea4bd4c8ac520187fc9daea39124769358e
Belen B <17783635+babm0930@users.noreply.github.com>
2020-08-01 22:48:31 +0200

    initial commit

---
+++ src/main/java/com/babm0930/birdPj/App.java
@@ -0,0 +1,13 @@
+package com.babm0930.birdPj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/babm0930/birdPj/AppTest.java
@@ -0,0 +1,20 @@
+package com.babm0930.birdPj;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
